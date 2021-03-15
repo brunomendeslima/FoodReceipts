79eef4879342dab44fd157463202f1c3ccf71ee3
Bruno Mendes Lima <bruno.lima@cwi.com.br>
2021-03-15 20:49:44 -0300

    Cadastro de ingredientes - sem banco ainda

---
+++ FoodRecipes/Data/FoodRecipesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FoodRecipes.Models;
+
+namespace FoodRecipes.Data
+{
+    public class FoodRecipesContext : DbContext
+    {
+        public FoodRecipesContext (DbContextOptions<FoodRecipesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FoodRecipes.Models.Ingredient> Ingredient { get; set; }
+    }
+}
--- FoodRecipes/Startup.cs
+++ FoodRecipes/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FoodRecipes.Data;
 
 namespace FoodRecipes
 {
@@ -33,6 +35,9 @@ namespace FoodRecipes
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<FoodRecipesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("FoodRecipesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
