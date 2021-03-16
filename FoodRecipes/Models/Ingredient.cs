0d92dd966c4a33d65d417a1ea26bae146261deec
Bruno Mendes Lima <bruno.lima@cwi.com.br>
2021-03-15 22:28:30 -0300

    ajustando o layout para pt-br

--- FoodRecipes/Models/Ingredient.cs
+++ FoodRecipes/Models/Ingredient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,13 @@ namespace FoodRecipes.Models
     public class Ingredient
     {
         public int Id { get; set; }
+        [DisplayName("Nome")]
         public string Name { get; set; }
+        [DisplayName("Preço por KG")]
         public decimal PriceUnit { get; set; }
+        [DisplayName("Quantidade")]
         public int Quantity { get; set; }
+        [DisplayName("Custo na receita")]
         public decimal PriceInRecipe { get; set; }
     }
 }
